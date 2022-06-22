3439abd6f298b65f6b03a56976c214af010dfa44
matteo <matciotola@gmail.com>
2022-06-22 14:40:40 +0200

    [STYLE] Minor change

--- MATLAB/consistency_metrics_evaluation.m
+++ MATLAB/consistency_metrics_evaluation.m
@@ -9,17 +9,16 @@
 %           I_F:                Fused Image;
 %           I_GT:               Ground-Truth image;
 %           ratio:              Scale ratio between MS and PAN. Pre-condition: Integer value;
-%           L:                  Image radiometric resolution; 
 %           Q_blocks_size:      Block size of the Q-index locally applied;
 %           flag_cut_bounds:    Cut the boundaries of the viewed Panchromatic image;
 %           dim_cut:            Define the dimension of the boundary cut;
 %
 % Outputs:
+%           Q2n_index:          Q2n index.
 %           Q_index:            Q index;
 %           SAM_index:          Spectral Angle Mapper (SAM) index;
 %           ERGAS_index:        Erreur Relative Globale Adimensionnelle de Synthèse (ERGAS) index;
-%           sCC:                spatial Correlation Coefficient between fused and ground-truth images;
-%           Q2n_index:          Q2n index.
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [Q2n_index, Q_index, SAM_index, ERGAS_index] = indexes_evaluation(I_F,I_GT,ratio,Q_blocks_size,flag_cut_bounds,dim_cut)
 
